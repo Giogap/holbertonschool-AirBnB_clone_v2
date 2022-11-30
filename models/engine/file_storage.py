932c2af8f4c7cf35ecc118c7b0f2bb57cfaff46a
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 <giogap@gmail.com>
2022-11-30 11:39:11 -0500

    5-delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if (not cls):
+            return FileStorage.__objects
+        new_obj = {}
+        for key, value in self.__objects.items():
+            if (cls.__name__ == value.to_dict()['__class__']):
+                new_obj[key] = value
+        return (new_obj)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
