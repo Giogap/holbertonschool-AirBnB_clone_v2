0cc3c3c873cfa4f777d8a8884e0bf05d75d69a6e
Giogap <giogap@gmail.com>
2022-12-02 12:28:03 -0500

    upload state

--- models/state.py
+++ models/state.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel, Base):
+HBNB_TYPE_STORAGE = os.environ['HBNB_TYPE_STORAGE']
+
+
+class State(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object): 
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            from models.city import City
+            from models import storage
+            city_obj = storage.all(City)
+            return [city for city in city_obj.values() if city.state_id == self.id]
