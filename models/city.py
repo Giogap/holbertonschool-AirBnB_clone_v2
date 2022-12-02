d71b0954ead23b4c1668ac76d85c5bf7d780f830
Giogap <giogap@gmail.com>
2022-12-02 12:34:14 -0500

    upload city

--- models/city.py
+++ models/city.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
+import os
 
+HBNB_TYPE_STORAGE = os.environ['HBNB_TYPE_STORAGE']
 
-class City(BaseModel, Base):
+class City(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object): 
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'),
+                        nullable=False)
+        
+    else:
+        state_id = ""
+        name = ""
