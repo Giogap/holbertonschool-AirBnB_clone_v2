6778ea3251afe3d28f1798c6c6ee3d38706dafbb
Giogap <giogap@gmail.com>
2022-12-02 11:37:11 -0500

    upload b_m init

--- models/base_model.py
+++ models/base_model.py
@@ -24,11 +24,12 @@ class BaseModel:
             self.updated_at = datetime.now()
             
         else:
+            if '__class__' in kwargs.keys():
+                del kwargs['__class__']
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+                                                     '%Y-%m-%dT%H:%M:%S.%f')            
         self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -51,6 +52,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary.keys():
+            del (dictionary["_sa_instance_state"])
         return dictionary
 
     def delete(self):
