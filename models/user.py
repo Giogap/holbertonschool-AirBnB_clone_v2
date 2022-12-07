bf73a6d8aa8931bb8bf25735954980a09f9b1ea7
Giogap <giogap@gmail.com>
2022-12-06 19:09:14 -0500

    update place and user

--- models/user.py
+++ models/user.py
@@ -1,27 +1,18 @@
+
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import Base
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    '''User class'''
-    __tablename__ = "users"
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)  # guipwd error
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    places = relationship(
-        "Place",
-        backref='user',
-        cascade="all, delete",
-        passive_deletes=True)
-        
-    reviews = relationship(
-        "Review",
-        backref='user',
-        cascade="all, delete",
-        passive_deletes=True)
+    places = relationship('Place', backref='user')
+    reviews = relationship('Review', backref='user')
