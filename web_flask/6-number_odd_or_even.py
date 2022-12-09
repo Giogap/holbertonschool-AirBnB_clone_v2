663c2420ba959c2a312f9b49a5f2dbe1c456c2ef
Giogap <giogap@gmail.com>
2022-12-09 12:23:31 -0500

    upload 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,7 +42,7 @@ def number_template(n):
 app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd(n):
     """ display “n is a number” only if n is an integer"""
-    return(render_template("6-number_odd_or_even.html", n=n))
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == '__main__':
